371188f85a4ff6cd1abc3befcaceb529c199edda
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-09-05 10:59:58 +0200

    Add knitr to the pkgs to install

--- src/install_packages.R
+++ src/install_packages.R
@@ -1,6 +1,8 @@
 installed <- rownames(installed.packages())
 required <- c("dplyr", "readr", "purrr", "stringr", "here",
-              "glue", "readxl", "DBI", "RSQLite", "digest", "tidylog")
+              "glue", "readxl", "DBI", "RSQLite", "digest", "tidylog",
+              "knitr" # used to extract R code from Rmd in run_*.R files
+)
 if (!all(required %in% installed)) {
   install.packages(required[!required %in% installed])
 }
