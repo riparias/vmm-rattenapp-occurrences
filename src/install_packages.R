a5021768443caa126cbbce1101fc4b13ce957d20
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-09-05 11:03:04 +0200

    Add also testthat to pkgs to install

--- src/install_packages.R
+++ src/install_packages.R
@@ -1,7 +1,8 @@
 installed <- rownames(installed.packages())
 required <- c("dplyr", "readr", "purrr", "stringr", "here",
               "glue", "readxl", "DBI", "RSQLite", "digest", "tidylog",
-              "knitr" # used to extract R code from Rmd in run_*.R files
+              "knitr", # used to extract R code from Rmd in run_*.R files
+              "testthat" # used for running tests in test_dwc_occurrence.R
 )
 if (!all(required %in% installed)) {
   install.packages(required[!required %in% installed])
