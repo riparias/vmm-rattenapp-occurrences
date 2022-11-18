1746bdbcc134b420afbd467727d64e2d82ae4b75
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-11-18 14:56:19 +0100

    Remove suffix "_update" in var name: redundant

--- tests/test_dwc_occurrence.R
+++ tests/test_dwc_occurrence.R
@@ -5,7 +5,7 @@ library(dplyr)
 
 # read proposed new version of the DwC mapping
 occs_path <- here::here("data", "processed", "occurrence.csv")
-dwc_occurrence_update <- readr::read_csv(occs_path, guess_max = 10000)
+dwc_occurrence <- readr::read_csv(occs_path, guess_max = 10000)
 
 # tests
 testthat::test_that("Right columns in right order", {
@@ -40,22 +40,22 @@ testthat::test_that("Right columns in right order", {
     "scientificName",
     "kingdom"
   )
-  testthat::expect_equal(names(dwc_occurrence_update), columns)
+  testthat::expect_equal(names(dwc_occurrence), columns)
 })
 
 testthat::test_that("occurrenceID is always present and is unique", {
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$occurrenceID)))
-  testthat::expect_equal(length(unique(dwc_occurrence_update$occurrenceID)),
-                         nrow(dwc_occurrence_update))
+  testthat::expect_true(all(!is.na(dwc_occurrence$occurrenceID)))
+  testthat::expect_equal(length(unique(dwc_occurrence$occurrenceID)),
+                         nrow(dwc_occurrence))
 })
 
 testthat::test_that("eventID is always present", {
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$eventID)))
+  testthat::expect_true(all(!is.na(dwc_occurrence$eventID)))
 })
 
 testthat::test_that("if individualCount is NA, samplingProtocol is not rat trap", {
   testthat::expect_equal(
-    dwc_occurrence_update %>%
+    dwc_occurrence %>%
       dplyr::filter(is.na(individualCount)) %>%
       dplyr::distinct(samplingProtocol) %>%
       dplyr::arrange(samplingProtocol) %>%
@@ -67,7 +67,7 @@ testthat::test_that("if individualCount is NA, samplingProtocol is not rat trap"
 testthat::test_that(
   "individualCount is > 0: samplingProtocol = 'casual observation' or 'rat trap'", {
     testthat::expect_equal(
-      dwc_occurrence_update %>%
+      dwc_occurrence %>%
         dplyr::filter(individualCount > 0) %>%
         dplyr::distinct(samplingProtocol) %>%
         dplyr::arrange(samplingProtocol) %>%
@@ -77,12 +77,12 @@ testthat::test_that(
   })
 
 testthat::test_that("recordedBy is always filled in", {
-  testthat::expect_true(all(!is.na(unique(dwc_occurrence_update$recordedBy))))
+  testthat::expect_true(all(!is.na(unique(dwc_occurrence$recordedBy))))
 })
 
 testthat::test_that("individualCount is never  0", {
   testthat::expect_equal(
-    dwc_occurrence_update %>%
+    dwc_occurrence %>%
       dplyr::filter(individualCount == 0) %>%
       nrow(),
     0
@@ -91,7 +91,7 @@ testthat::test_that("individualCount is never  0", {
 
 testthat::test_that("occurrenceRemarks values", {
   testthat::expect_equal(
-    dwc_occurrence_update %>%
+    dwc_occurrence %>%
       dplyr::distinct(occurrenceRemarks) %>%
       arrange(occurrenceRemarks) %>%
       pull(),
@@ -100,33 +100,33 @@ testthat::test_that("occurrenceRemarks values", {
 })
 
 testthat::test_that("eventID is always filled in", {
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$eventID)))
+  testthat::expect_true(all(!is.na(dwc_occurrence$eventID)))
 })
 
 testthat::test_that("eventDate is always filled in", {
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$eventDate)))
+  testthat::expect_true(all(!is.na(dwc_occurrence$eventDate)))
 })
 
 testthat::test_that("locationID is always filled in", {
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$locationID)))
+  testthat::expect_true(all(!is.na(dwc_occurrence$locationID)))
 })
 
 testthat::test_that("decimalLatitude is always filled in", {
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$decimalLatitude)))
+  testthat::expect_true(all(!is.na(dwc_occurrence$decimalLatitude)))
 })
 
 testthat::test_that("decimalLatitude is within Flemish boundaries", {
-  testthat::expect_true(all(dwc_occurrence_update$decimalLatitude < 51.65))
-  testthat::expect_true(all(dwc_occurrence_update$decimalLatitude > 50.63))
+  testthat::expect_true(all(dwc_occurrence$decimalLatitude < 51.65))
+  testthat::expect_true(all(dwc_occurrence$decimalLatitude > 50.63))
 })
 
 testthat::test_that("decimalLongitude is always filled in", {
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$decimalLongitude)))
+  testthat::expect_true(all(!is.na(dwc_occurrence$decimalLongitude)))
 })
 
 testthat::test_that("decimalLongitude is within Flemish boundaries", {
-  testthat::expect_true(all(dwc_occurrence_update$decimalLongitude < 5.95))
-  testthat::expect_true(all(dwc_occurrence_update$decimalLongitude > 2.450))
+  testthat::expect_true(all(dwc_occurrence$decimalLongitude < 5.95))
+  testthat::expect_true(all(dwc_occurrence$decimalLongitude > 2.450))
 })
 
 
@@ -176,6 +176,6 @@ testthat::test_that("scientificName is never NA and one of the list", {
     "Decapoda",
     "Martes martes"
   )
-  testthat::expect_true(all(!is.na(dwc_occurrence_update$scientificName)))
-  testthat::expect_true(all(dwc_occurrence_update$scientificName %in% species))
+  testthat::expect_true(all(!is.na(dwc_occurrence$scientificName)))
+  testthat::expect_true(all(dwc_occurrence$scientificName %in% species))
 })
