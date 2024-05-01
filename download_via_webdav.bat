a69c3763c92977bbf09c43943b582372375a0bac
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-05-01 22:17:48 +0200

    Move download pictures at the end

--- download_via_webdav.bat
+++ download_via_webdav.bat
@@ -3,12 +3,14 @@ set arg1=%1
 net use w: "https://int-web.vmm.be/ratten" %arg1% /user:INBO
 w:
 copy *.csv C:\Users\damiano_oldoni\Documents\GitHub\vmm-rattenapp-occurrences\data\raw
-copy Fotos\*.jpg C:\Users\damiano_oldoni\Documents\GitHub\vmm-rattenapp-occurrences\data\raw\pictures
 c:
-net use w: /delete
 cd C:\Users\damiano_oldoni\Documents\GitHub\vmm-rattenapp-occurrences\data\raw
 move /y "Dieren en Planten Waarnemingen INBO Email-nl-be.csv" "Dieren en Planten Waarnemingen from 2022-01-01.csv"
 move /y "Vangsten INBO Email-nl-be.csv" "Vangsten from 2022-01-01.csv"
 cd..
 cd..
+w:
+copy Fotos\*.jpg C:\Users\damiano_oldoni\Documents\GitHub\vmm-rattenapp-occurrences\data\raw\pictures
+net use w: /delete
+
 
