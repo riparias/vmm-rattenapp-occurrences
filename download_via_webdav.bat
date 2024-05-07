aef21bc1a39b58ac53818e39d7a570932b746846
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-05-07 10:06:21 +0200

    Import only new pictures to save time

--- download_via_webdav.bat
+++ download_via_webdav.bat
@@ -10,7 +10,5 @@ move /y "Vangsten INBO Email-nl-be.csv" "Vangsten from 2022-01-01.csv"
 cd..
 cd..
 w:
-copy Fotos\*.jpg C:\Users\damiano_oldoni\Documents\GitHub\vmm-rattenapp-occurrences\data\raw\pictures
+echo n | copy /-y Fotos\*.jpg C:\Users\damiano_oldoni\Documents\GitHub\vmm-rattenapp-occurrences\data\raw\pictures
 net use w: /delete
-
-
