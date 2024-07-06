37041aa0c081ce315d090c86ae2d19538c49e75a
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-07-06 14:45:06 +0200

    Add Procambarus to species list in test

--- tests/test_dwc_occurrence.R
+++ tests/test_dwc_occurrence.R
@@ -214,7 +214,8 @@ testthat::test_that("scientificName is never NA and one of the list", {
     "Crassula helmsii",
     "Pontederia cordata",
     "Houttuynia cordata",
-    "Mimulus guttatus DC."
+    "Mimulus guttatus DC.",
+    "Procambarus"
   )
   testthat::expect_true(all(!is.na(dwc_occurrence$scientificName)))
   testthat::expect_true(all(dwc_occurrence$scientificName %in% species))
