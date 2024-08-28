2bbdc0c96fc8d005da1d68c1fa71968d6f9d880c
Damiano Oldoni <damiano.oldoni@inbo.be>
2024-08-28 15:23:01 +0200

    Adjust mapping

--- sql/dwc_occurrence.sql
+++ sql/dwc_occurrence.sql
@@ -217,6 +217,7 @@ WHERE
   o."Sporen Waarnemingen Naam" != 'Andere' AND
   o."Sporen Waarnemingen Naam" != 'Nest' AND
   o."Sporen Waarnemingen Naam" != 'Muizen' AND
+  o."Sporen Waarnemingen Naam" != 'Wolf' AND
   o."Team Naam" != 'Controle Oost' AND
   o."Team Naam" != 'Controle West'
 ORDER BY
