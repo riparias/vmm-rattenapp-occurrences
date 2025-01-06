1ce581ab6796fa1faa7e74cb8db8007fa4e728ba
Damiano Oldoni <damiano.oldoni@inbo.be>
2025-01-06 10:39:24 +0100

    Add mapping of "Rode ibis"

--- sql/dwc_occurrence.sql
+++ sql/dwc_occurrence.sql
@@ -168,6 +168,7 @@ SELECT
     WHEN o."Sporen Waarnemingen Naam" = '11 amerikaanse rivierkree' THEN 'Procambarus'
     WHEN o."Sporen Waarnemingen Naam" = 'Amerikaanse rivierkree 10' THEN 'Procambarus'
     WHEN o."Sporen Waarnemingen Naam" = 'Amerikaanse rivierkreek' THEN 'Procambarus'
+    WHEN o."Sporen Waarnemingen Naam" = 'Rode ibis' THEN 'Eudocimus ruber'
     ELSE NULL
   END                                   AS scientificName,
   CASE
