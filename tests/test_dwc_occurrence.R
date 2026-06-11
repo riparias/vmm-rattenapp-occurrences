882bc0cdad202e3d9ec5a8107c99ca19f734cdef
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-06-11 09:39:08 +0200

    Remove check instruction prolbems()

--- tests/test_dwc_occurrence.R
+++ tests/test_dwc_occurrence.R
@@ -6,7 +6,6 @@ library(dplyr)
 # read proposed new version of the DwC mapping
 occs_path <- here::here("data", "processed", "occurrence.csv")
 dwc_occurrence <- readr::read_csv(occs_path, guess_max = 50000)
-problems(dwc_occurrence)
 
 View(dwc_occurrence)
 # tests
